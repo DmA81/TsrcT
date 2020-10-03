1aa2c744293435abbc73b194d1d155b389e8bdc1
DmA81 <dario.mango@gmail.com>
2020-10-03 12:12:51 +0200

    fixed esteps

--- config/marlin-2.0/Configuration.h
+++ config/marlin-2.0/Configuration.h
@@ -735,7 +735,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 348.18 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 406.31 }
 
 /**
  * Default Max Feed Rate (mm/s)
